8c217aef14b7ed11c73a31b43066c86eb76cc5ba
Paul <vmpatoleia@protonmail.com>
2018-09-12 17:59:24 +0000

    Install all packages needed for auth | Define User Model

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var express       = require('express'),
+var express       = require("express"),
     app           = express(),
-    bodyParser    = require('body-parser'),
+    bodyParser    = require("body-parser"),
     mongoose      = require("mongoose"),
+    passport      = require("passport"),
+    LocalStrategy = require("passport-local"),
     
-    Campground    = require('./models/campground'),
-    Comment       = require('./models/comment'),
+    Campground    = require("./models/campground"),
+    Comment       = require("./models/comment"),
+    User          = require("./models/user"),
     seedDB        = require("./seeds")
     
     
