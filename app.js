f373f71a6d42957a58cd39fdb12a6a4985b1f11f
Paul <vmpatoleia@protonmail.com>
2018-09-15 15:04:29 +0000

    Configure passport, add register routes, add register template

--- app.js
+++ app.js
@@ -22,6 +22,18 @@ app.use(express.static(__dirname + '/public'))
 
 seedDB()
 
+// PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "Once again rusty cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 app.get('/', function(req, res) {
     res.render('landing')
 })
@@ -102,6 +114,27 @@ app.post('/campgrounds/:id/comments', function(req, res) {
                 }
             })
         }
+    })
+})
+
+
+// AUTH ROUTES
+
+//show register form
+
+app.get("/register", function(req, res) {
+    res.render("register")
+})
+
+app.post("/register", function(req, res) {
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+        if(err) {
+            console.log(err)
+            return res.render('register')
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds")
+        })
     })
 })
 
